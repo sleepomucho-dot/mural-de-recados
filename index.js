21c24e197df0c54b43670a2f789e40144c92aa40
sleepomucho-dot <sleepomucho@gmail.com>
2026-09-14 15:19:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+const path = require ('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
